9fa502e0c9848627a1031b26718b3bb30c5bf1ee
dir-FY24fujiiyuzuki <yuzuki.fujii.p795@dirbato.co.jp>
2024-04-24 23:56:25 +0900

    DBUtil作成

--- daily_report_system/src/main/java/utils/DBUtil.java
+++ daily_report_system/src/main/java/utils/DBUtil.java
@@ -7,4 +7,21 @@ import constants.JpaConst;
 
 public class DBUtil {
 
-}
+    private static EntityManagerFactory emf;
+       
+    
+  //EntityManagerインスタンスを生成
+    public static EntityManager createEntityManager() {
+        return _getEntityManagerFactory().createEntityManager();
+    }
+  //EntityManagerFactoryインスタンスを生成
+    private static EntityManagerFactory _getEntityManagerFactory() {
+        if (emf == null) {
+            emf = Persistence.createEntityManagerFactory(JpaConst.PERSISTENCE_UNIT_NAME);
+        }
+
+        return emf;
+    }
+    
+    }
+
