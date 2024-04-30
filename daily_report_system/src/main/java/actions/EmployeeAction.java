c198b8ad6f148ddcfe4677c8d49ec1d2952e85f1
dir-FY24fujiiyuzuki <yuzuki.fujii.p795@dirbato.co.jp>
2024-04-30 12:04:59 +0900

    管理者チェックを追加

--- daily_report_system/src/main/java/actions/EmployeeAction.java
+++ daily_report_system/src/main/java/actions/EmployeeAction.java
@@ -40,7 +40,8 @@ public class EmployeeAction extends ActionBase {
      * @throws IOException
      */
     public void index() throws ServletException, IOException {
-
+        //管理者かどうかのチェック
+        if(checkAdmin()) {
         //指定されたページ数の一覧画面に表示するデータを取得
         int page = getPage();
         List<EmployeeView> employees = service.getPerPage(page);
@@ -62,7 +63,7 @@ public class EmployeeAction extends ActionBase {
 
         //一覧画面を表示
         forward(ForwardConst.FW_EMP_INDEX);
-
+        }
     }
     /**
      * 新規登録画面を表示する
@@ -70,21 +71,23 @@ public class EmployeeAction extends ActionBase {
      * @throws IOException
      */
     public void entryNew() throws ServletException, IOException {
-
+        //管理者が同課のチェック
+        if(checkAdmin()) {
         putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
         putRequestScope(AttributeConst.EMPLOYEE, new EmployeeView()); //空の従業員インスタンス
 
         //新規登録画面を表示
         forward(ForwardConst.FW_EMP_NEW);
+        }
     }
-
     /**
      * 新規登録を行う
      * @throws ServletException
      * @throws IOException
      */
     public void create() throws ServletException, IOException {
-
+     //管理者かどうかのチェック
+        if(checkAdmin()) {
         //CSRF対策 tokenのチェック
         if (checkToken()) {
           //パラメータの値を元に従業員情報のインスタンスを作成する
@@ -122,7 +125,7 @@ public class EmployeeAction extends ActionBase {
                 //一覧画面にリダイレクト
                 redirect(ForwardConst.ACT_EMP, ForwardConst.CMD_INDEX);
             }
-
+        }
         }
     }
     /**
@@ -131,7 +134,8 @@ public class EmployeeAction extends ActionBase {
      * @throws IOException
      */
     public void show() throws ServletException, IOException {
-
+        //管理者かどうかのチェック
+        if(checkAdmin()) {
         //idを条件に従業員データを取得する
         EmployeeView ev = service.findOne(toNumber(getRequestParam(AttributeConst.EMP_ID)));
 
@@ -146,6 +150,7 @@ public class EmployeeAction extends ActionBase {
 
         //詳細画面を表示
         forward(ForwardConst.FW_EMP_SHOW);
+        }
     }
     /**
      * 編集画面を表示する
@@ -153,7 +158,8 @@ public class EmployeeAction extends ActionBase {
      * @throws IOException
      */
     public void edit() throws ServletException, IOException {
-
+        //管理者かどうかのチェック
+        if(checkAdmin()) {
         //idを条件に従業員データを取得する
         EmployeeView ev = service.findOne(toNumber(getRequestParam(AttributeConst.EMP_ID)));
 
@@ -169,7 +175,7 @@ public class EmployeeAction extends ActionBase {
 
         //編集画面を表示する
         forward(ForwardConst.FW_EMP_EDIT);
-
+        }
     }   
     
     /**
@@ -178,7 +184,8 @@ public class EmployeeAction extends ActionBase {
      * @throws IOException
      */
     public void update() throws ServletException, IOException {
-
+        //管理者かどうかのチェック
+        if(checkAdmin()) {
         //CSRF対策 tokenのチェック
         if (checkToken()) {
             //パラメータの値を元に従業員情報のインスタンスを作成する
@@ -217,13 +224,15 @@ public class EmployeeAction extends ActionBase {
                 redirect(ForwardConst.ACT_EMP, ForwardConst.CMD_INDEX);
             }
         }
+        }
     }/**
      * 論理削除を行う
      * @throws ServletException
      * @throws IOException
      */
     public void destroy() throws ServletException, IOException {
-
+        //管理者かどうかのチェック
+        if(checkAdmin()) {
         //CSRF対策 tokenのチェック
         if (checkToken()) {
 
@@ -236,6 +245,33 @@ public class EmployeeAction extends ActionBase {
             //一覧画面にリダイレクト
             redirect(ForwardConst.ACT_EMP, ForwardConst.CMD_INDEX);
         }
+        }
     }
+    
+    /**
+     * ログイン中の従業員が管理者かどうかチェックし、管理者でなければエラー画面を表示
+     * true: 管理者 false: 管理者ではない
+     * @throws ServletException
+     * @throws IOException
+     */
+    private boolean checkAdmin() throws ServletException, IOException {
+
+        //セッションからログイン中の従業員情報を取得
+        EmployeeView ev = (EmployeeView) getSessionScope(AttributeConst.LOGIN_EMP);
+
+        //管理者でなければエラー画面を表示
+        if (ev.getAdminFlag() != AttributeConst.ROLE_ADMIN.getIntegerValue()) {
+
+            forward(ForwardConst.FW_ERR_UNKNOWN);
+            return false;
+
+        } else {
+
+            return true;
+        }
+
+    }
+
+
     
         }
